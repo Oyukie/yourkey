4c657d527341f7ed6f07bbc09b770b9a5da3854e
Oyukie <86396182+Oyukie@users.noreply.github.com>
2021-06-24 15:46:41 +0800

    Delete trx.sh

--- trx.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-POOL=ethash-us.unmineable.com:3333
-WALLET=SHIB:0xd2b8488eF94b4F16C6fCacA8dfBde6C3DFdC38c
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-leka)
-
-chmod +x tuyulgpu
-./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
