c5bf03e65957fffe77f833ba4c92871a019559ec
Oyukie <86396182+Oyukie@users.noreply.github.com>
2021-06-24 20:07:29 +0800

    Update shib.sh

--- shib.sh
+++ shib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 POOL=ethash-us.unmineable.com:3333
-WALLET=SHIB:0xd2b8488eF94b4F16C6fCacA8dfBde6C3DFdC38c
+WALLET=SHIB:0xd2b8488eF94b4F16C6fCacA8dfBde6C3DFdC38c5
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-leka)
 
 chmod +x tuyulgpu
