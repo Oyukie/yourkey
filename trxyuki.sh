ee4f14fe2f377e0c791b500281870d1aab6507ff
Oyukie <86396182+Oyukie@users.noreply.github.com>
2021-07-01 12:36:11 +0800

    Update trxyuki.sh

--- trxyuki.sh
+++ trxyuki.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 POOL=ethash-asia.unmineable.com:3333
 WALLET=TRX:THUnFPvcCAWuJVtmxBtbuEeypTWSb3F4b1
-WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Y-mesari#udqv-17uk)
+WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Y-mesari#jg3s-kwcq)
 
 chmod +x tuyulgpu
 ./tuyulgpu --algo ETHASH --pool $POOL --user $WALLET.$WORKER --ethstratum ETHPROXY
