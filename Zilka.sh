c92a8e774359baf52d1ade320da863e7878f0ee5
Oyukie <86396182+Oyukie@users.noreply.github.com>
2021-07-02 23:53:29 +0800

    Update Zilka.sh

--- Zilka.sh
+++ Zilka.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=ethash-asia.unmineable.com:3333
+POOL=ethash-us.unmineable.com:3333
 WALLET=ZIL:zil1m4sdk9a7zmal607faeahqutg9m07s0s7fh58js
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-yowis#6vjg-gx4b)
 
