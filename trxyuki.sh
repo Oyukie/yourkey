5d67b600c8d13e95d6a336d961dad59a8a663568
Oyukie <86396182+Oyukie@users.noreply.github.com>
2021-07-03 12:53:18 +0800

    Update trxyuki.sh

--- trxyuki.sh
+++ trxyuki.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-POOL=ethash-us.unmineable.com:3333
+POOL=ethash-asia.unmineable.com:3333
 WALLET=TRX:THUnFPvcCAWuJVtmxBtbuEeypTWSb3F4b1
 WORKER=$(echo $(shuf -i 1000-9999 -n 1)-Y-mesari#jg3s-kwcq)
 
